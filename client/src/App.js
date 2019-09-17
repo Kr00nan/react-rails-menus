83126cc76d5116805ec31d08f349c2ee0c0ae19a
Michael Kroon <kroonology@gmail.com>
2019-09-16 22:24:05 -0600

    import semantic; stub out app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import ItemForm from './components/ItemForm';
+import ItemList from './components/ItemList';
+import {Container} from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  state = {items: []};
 
-    </div>
-  );
-}
+  componentDidMount() {
+    // make a call to our rails server to get menu items
+  }
+
+  addItem = (name) => {
+    // make api call to rails server to add item
+    // update state
+  }
+
+  updateItem = (id) => {
+    // make api call to rails server to add item
+    // update state
+  }
+
+  deleteItem = (id) => {
+    // make api call to delete item
+    // remove it from state
+  }
+
+  render() {
+    return (
+      <Container style={{padding: '30px 0'}}>
+        <ItemForm addItem={this.addItem} />
+        <br />
+        <br />
+        <ItemList
+          items={this.state.items}
+          updateItem={this.updateItem}
+          deleteItem={this.deleteItem}
+        />
+      </Container>
+    )
+  }
+
+};
 
 export default App;
