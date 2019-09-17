bb7e02c50eb4951f63b54bbc91524e66439d81b8
Michael Kroon <kroonology@gmail.com>
2019-09-16 22:04:12 -0600

    CRUD on menu_item controller

--- app/controllers/api/menu_items_controller.rb
+++ app/controllers/api/menu_items_controller.rb
@@ -1,2 +1,36 @@
 class Api::MenuItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index
+    render json: MenuItem.all
+
+  end
+
+  def create
+    item = MenuItem.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: {errors: item.errors}, status: :unprocessable_entity
+  end
+
+  def update
+    @item.update(item_params)
+    render json: @item
+
+  end
+
+  def destroy
+    @item.destroy
+    render json: {message: 'Item deleted'}
+  end
+
+  private
+  def item_params
+    params.require(:menu_item).permit(:name, :meal_type)
+  end
+
+  def set_item
+    @item = MenuItem.find(params[:id])
+  end
 end
